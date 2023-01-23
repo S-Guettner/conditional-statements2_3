2a547ed2330bccb616bbf9923fdb3af5537161c9
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-23 13:07:22 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let calculate = (inputNumber) =>{
+    let difference = inputNumber -27;
+    if(difference > 27){
+        console.log(inputNumber +"-27 = "+ " " + difference);
+        console.log(difference + " "  + " * 2 = " + difference * 2);
+    }else{
+        console.log(inputNumber + "-27" + " " + "= " +difference);
+    }
+}
